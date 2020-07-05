0a1c6bef2d041303357b63e6523acccbcb2dbcea
abuchachos <45379148+abuchachos@users.noreply.github.com>
2020-07-05 12:20:54 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
 	"shortDomain": "go.param.me",
 	"defaultRedirect": "https://www.param.me",
-	"repo": "paramt/go.param.me",
-	"users": ["paramt"],
+	"repo": "abuchachos/go.param.me",
+	"users": ["abuchachos"],
 	"netlify_redirects": true,
 	"utm": false
 }
