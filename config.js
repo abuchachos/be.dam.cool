6e42613eb2c78a5938c83f667c799c1eab4bdcb0
abuchachos <45379148+abuchachos@users.noreply.github.com>
2021-01-13 21:22:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
 	"shortDomain": "be.dam.cool",
 	"defaultRedirect": "https://www.duck.com",
-	"repo": "abuchachos/go.param.me",
-	"users": ["abuchachos"],
+	"repo": "abuchachos/be.dam.cool",
+	"users": "abuchachos",
 	"netlify_redirects": true,
 	"utm": false
 }
