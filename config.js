2a653acc9da9e0f92e062f9b9d20c97d7114b09a
abuchachos <45379148+abuchachos@users.noreply.github.com>
2021-02-20 13:04:31 -0500

    Bracket

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
 	"shortDomain": "be.dam.cool",
 	"defaultRedirect": "https://www.duck.com",
 	"repo": "abuchachos/be.dam.cool",
-	"users": "abuchachos",
+	"users": ["abuchachos"],
 	"netlify_redirects": true,
 	"utm": false
 }
